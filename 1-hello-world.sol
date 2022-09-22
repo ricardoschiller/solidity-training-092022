8a9128a38a063669ad4d73046ccf4a49716c8b57
Ricardo Pinto <ricardoschillermail@gmail.com>
2022-09-22 20:13:46 +0100

    Updated comments to hello world

--- 1-hello-world.sol
+++ 1-hello-world.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
+// pragma solidity 0.6.12  - Only compiles with version 0.6.12
+// pragma solidity ^0.6.12 - Compiles with version 0.6.12 and above up until 0.7 (not including)
+// pragma solidity >=0.4.0 <0.6.0 - Compiles with all versions between 0.4.0 and 0.6.0
 pragma solidity ^0.8.7;
 
 contract HelloWorld {
